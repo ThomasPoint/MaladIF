e8767608dca2d6b2d44f85df276762df5f3f5c04
romainfontaine <romain.fontaine73@gmail.com>
2017-04-11 18:06:07 +0200

    Début de parsing d'arguments

--- MaladIFServeur/MaladIFServeur.cpp
+++ MaladIFServeur/MaladIFServeur.cpp
@@ -6,6 +6,20 @@ using namespace System;
 
 int main(array<System::String ^> ^args)
 {
-    Console::WriteLine(L"Hello World");
+    Console::WriteLine(args->Length);
+	int port = 4242;
+	switch (args->Length) {
+	case 3:
+		Int32::TryParse(args[2], port);
+	case 1:
+		Console::WriteLine(args[0]);
+		Console::WriteLine(port);
+		break;
+	default:
+		//erreur
+		break;
+	}
+
+	Console::ReadKey();
     return 0;
 }
